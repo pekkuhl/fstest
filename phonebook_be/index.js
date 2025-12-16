0fb5038fa2fce69b152bd85a71078c439126b60b
Your Name <you@example.com>
2025-12-16 12:36:33 +0200

    static dist korjattu

--- phonebook_be/index.js
+++ phonebook_be/index.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const app = express()
 app.use(express.json())
-
+app.use(express.static('dist'))
 
 const generateId = () => {
     const maxId = persons.length > 0
